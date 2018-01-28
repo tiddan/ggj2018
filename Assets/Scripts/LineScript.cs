1f3b297afc76f512f6e74923db94e541d34dc438
TiddanVR <tiddan@gmail.com>
2018-01-28 10:58:35 +0100

    Fixed linerenderer

--- Assets/Scripts/LineScript.cs
+++ Assets/Scripts/LineScript.cs
@@ -21,19 +21,21 @@ public class LineScript : MonoBehaviour
 
 	    if (isActive)
 	    {
-	        lineRenderer.material.mainTextureOffset = new Vector2(Time.timeSinceLevelLoad, 0);
+	        lineRenderer.material.mainTextureOffset = new Vector2(-Time.timeSinceLevelLoad*10.0f, 0);
 	    }
 	}
 
     public void Configure(Vector3 origin, Vector3 destination, string team)
     {
-        var d = Vector3.Distance(Origin, Destination);
+        //var d = Vector3.Distance(Origin, Destination);
 
         Origin = origin;
         Destination = destination;
-        lineRenderer.SetPositions(new [] { Origin, Destination});
-        lineRenderer.material.mainTextureScale = new Vector2(d,1.0f);
-        Vector3.Distance(Origin, Destination);
+        lineRenderer.SetPositions(new [] { Origin, new Vector3(Destination.x, Destination.y+6.7f,Destination.z), });
+        lineRenderer.material.mainTextureScale = new Vector2(50.0f,1.0f);
+        //lineRenderer.material.color = team == "RED" ? Color.red : Color.blue;
+        lineRenderer.material.SetColor("_TintColor", team == "RED" ? Color.red : Color.blue);
+        
         isActive = true;
     }
 }
